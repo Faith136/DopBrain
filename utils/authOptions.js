f2ac41567364950afba89b4a038ae447de315028
Faith136 <faithodongo9@gmail.com>
2025-10-11 22:12:30 +0300

    auth

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,31 +1,36 @@
-import CredentialsProvider from 'next-auth/providers/credentials';
-import User from '@/model/User';
-import bcrypt from 'bcrypt';
-import dbConnect from '@/utils/dbConnect';
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions = {
-session: {
-    strategy: 'jwt',
+  session: {
+    strategy: "jwt",
   },
   providers: [
     CredentialsProvider({
-      async authorize(credentials, req) {
-      dbConnect();
+      async authorize(credentials) {
         const { email, password } = credentials;
+
+        // 👇 import here so it's not run during build
+        const dbConnect = (await import("@/utils/dbConnect")).default;
+        const User = (await import("@/model/User")).default;
+
+        await dbConnect();
         const user = await User.findOne({ email });
         if (!user) {
-          throw new Error("invalid email");
+          throw new Error("Invalid email");
         }
+
         const isValid = await bcrypt.compare(password, user.password);
         if (!isValid) {
           throw new Error("Invalid password");
         }
+
         return user;
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,    //later when the user tries to access to a page that requires authenticated user they will be redirected to the login page
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: "/login"
-  }
-}
+    signIn: "/login",
+  },
+};
